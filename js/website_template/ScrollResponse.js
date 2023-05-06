884abecdc4f2bf256e57be3bb13c8f156eb5e858
rcph <rcph110110@gmail.com>
2023-05-06 19:32:04 +0800

    updated_origin

--- js/website_template/ScrollResponse.js
+++ js/website_template/ScrollResponse.js
@@ -8,7 +8,7 @@ class ScrollResponse {
     const elements = document.querySelectorAll(querySelector)
     const offsets = []
     elements.forEach(element => {
-      let offset = {element: element, top: 0, left: 0, right: 0, bottom: 0, width: 0, height: 0}
+      let offset = { element: element, top: 0, left: 0, right: 0, bottom: 0, width: 0, height: 0, originX: 0, originY: 0 }
       const rect = element.getBoundingClientRect()
       offset.top = rect.top
       offset.left = rect.left
@@ -16,9 +16,12 @@ class ScrollResponse {
       offset.bottom = rect.bottom
       offset.width = rect.width
       offset.height = rect.height
-      
+      offset.originX = rect.left + ((rect.right - rect.left) / 2) // rect.left + (rect.width / 2)
+      offset.originY = rect.top + ((rect.bottom - rect.top) / 2) // rect.top + (rect.height / 2)
+
       offsets.push(offset)
     })
     return offsets
   }
+  static paralax
 }
